06a1ad4ec53cb718901ecf7661d33080a479575f
minhthong <minhthongbkhn1994@gmail.com>
2022-04-19 00:52:29 +0700

    update api v2

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-  return 'Hello World Jenkins <3 NestJs!';
+  return 'Hello World Jenkins <3 NestJs & Docker!';
   }
 }
