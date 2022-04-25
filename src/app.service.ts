36a2c3bb10abff958d31ce3b4d2a6e292409f18b
minhthong <minhthongbkhn1994@gmail.com>
2022-04-25 22:29:44 +0700

    update example v1

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-  return 'Hello World Jenkins <3 NestJs & Docker! 123';
+  return 'Hello World Jenkins <3 NestJs & Docker! 123 111';
   }
 }
