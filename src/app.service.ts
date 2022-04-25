c387461bc91bb6f260e88dc5104842352bdfa2dd
minhthong <minhthongbkhn1994@gmail.com>
2022-04-26 02:19:32 +0700

    test merge PR 1

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-  return 'Hello World Jenkins <3 NestJs & Docker! Dev PR';
+  return 'Hello World Jenkins <3 NestJs & Docker! Dev PR GG';
   }
 }
