3ad0ba46c006c84cea8e2cf8a39dcb58882551bf
minhthong <minhthongbkhn1994@gmail.com>
2022-05-05 01:55:24 +0700

    update mysql config

--- src/application/core/configs/orm.config.ts
+++ src/application/core/configs/orm.config.ts
@@ -1,10 +1,10 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 export const ormConfig: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: 'mysql-76133-0.cloudclusters.net',
+  host: 'mysql-78002-0.cloudclusters.net',
   username: 'admin',
-  password: 'z0VTi5JF',
-  port: 12020,
+  password: 'o0c95j1g',
+  port: 14240,
   database: 'bruceleeblog',
   entities: ["dist/**/*.entity{.ts,.js}"],
   synchronize: false
