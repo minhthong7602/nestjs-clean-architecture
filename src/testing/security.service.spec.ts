4bb82a6c574ff03ac667788261bf70bf22c8a306
minhthong <minhthongbkhn1994@gmail.com>
2022-05-05 00:04:34 +0700

    auto check security service 3

--- src/testing/security.service.spec.ts
+++ src/testing/security.service.spec.ts
@@ -6,7 +6,7 @@ describe('Security Service', () => {
    it('encryptPassword: Check plain password same password enscript', async () => {
     const plainPassword = "12345678";
     const encryptObj = await securityService.encryptPassword(plainPassword);
-    expect(encryptObj.hash === plainPassword).toBeFalsy();
+    expect(encryptObj.hash === plainPassword).toBeTruthy();
    });
 
    it('encryptPassword & hashPassword: when user enter correct password', async () => {
